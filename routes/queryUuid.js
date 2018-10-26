58edc4f3b97e2786da81ec97a6ff36000b19057f
qihong1 <qihong1@lenovo.com>
2018-10-26 21:52:36 +0800

    asdf

--- routes/queryUuid.js
+++ routes/queryUuid.js
@@ -11,15 +11,43 @@ var JWT_PASSWORD = 'token';
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: '39.106.140.80',
-	user: 'root',
-	password: 'Qihong38752673',
-	database: 'saoyisao'
-});
+function handleError(err) {
+	if (err) {
+		// 如果是连接断开，自动重新连接
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			connect();
+		} else {
+			console.error(err.stack || err);
+		}
+	}
+}
+
+// 连接数据库
+function connect() {
+	db = mysql.createConnection({
+		host: '39.106.140.80',
+		user: 'root',
+		password: 'Qihong38752673',
+		database: 'saoyisao'
+	});
+	db.connect(handleError);
+	db.on('error', handleError);
+}
+
+
+
+// var connection = mysql.createConnection({
+// 	host: '39.106.140.80',
+// 	user: 'root',
+// 	password: 'Qihong38752673',
+// 	database: 'saoyisao'
+// });
 
 //执行创建连接 
-connection.connect();
+// connection.connect();
+
+var db;
+connect();
 
 router.post('/', bodyParser.json(), function(req, res, next) {
 
@@ -27,7 +55,7 @@ router.post('/', bodyParser.json(), function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*"); //设置跨域访问 
 	var sql = "SELECT * FROM `saotoken` WHERE uuid='" + req.body.uuid + "'";
 
-	connection.query(sql, function(err, result) {
+	db.query(sql, function(err, result) {
 
 		if (err == null) {
 			res.json({
