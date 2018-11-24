8fdaad212a9677f8cbda055cf4a67dd4214c441d
qihong1 <qihong1@lenovo.com>
2018-11-24 20:40:57 +0800

    asdf

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ var updateUuid = require('./routes/updateUuid');
 var queryUuid = require('./routes/queryUuid');
 
 
+var cname = require('./routes/cname');
+
+//文字转语音
+
+var textToAudio = require('./routes/textToAudio/textToAudio');
+
+
 
 let jwt = require('jsonwebtoken');
 var JWT_PASSWORD = 'token';
@@ -64,10 +71,13 @@ app.use('/updateUuid', updateUuid);
 
 app.use('/queryUuid', queryUuid);
 
+app.use('/textToAudio', textToAudio);
 
+
+app.use('/cname', cname);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
   console.log(req, res, next);
   var err = new Error('Not Found');
@@ -80,7 +90,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -91,7 +101,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
